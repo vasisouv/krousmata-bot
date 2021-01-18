04406bb1d977374148c4610457322a8059a8c5db
Vasilis Souvatzis <souvatzisbill@gmail.com>
2021-01-18 18:18:42 +0200

    changed data source

--- README.md
+++ README.md
@@ -25,4 +25,4 @@ TOKEN=[your bot token]
 
 ## Credits
 
-[Covid-19-Response-Greece](https://github.com/Covid-19-Response-Greece/covid19-greece-api) For the Covid-19 cases API.
+[Open Disease Data (disease.sh)](https://disease.sh/) For the Covid-19 cases API.
--- main.py
+++ main.py
@@ -13,32 +13,13 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def fetch_cases(endpoint):
-    r = requests.get(f'https://covid-19-greece.herokuapp.com/{endpoint}')
-    d = r.json()
-    cases = d['cases']
-    return cases
-
-
-def get_date_confirmed_and_deaths():
-    cases = fetch_cases('all')
-    last_known = cases[-1]
-    p_last_known = cases[-2]
-    confirmed = last_known['confirmed'] - p_last_known['confirmed']
-    deaths = last_known['deaths'] - p_last_known['deaths']
-    date = datetime.datetime.strptime(last_known['date'], '%Y-%m-%d').strftime("%d %b %Y")
-    return date, confirmed, deaths
-
-
-def get_intensive():
-    cases = fetch_cases('intensive-care')
-    return cases[-1]['intensive_care']
-
-
 def get_data():
-    date, confirmed, deaths = get_date_confirmed_and_deaths()
-    intensive = get_intensive()
-    return date, confirmed, deaths, intensive
+    data = requests.get('https://corona.lmao.ninja/v2/countries/greece?strict=true&query').json()
+    cases = data['todayCases']
+    deaths = data['todayDeaths']
+    critical = data['critical']
+    date = datetime.datetime.fromtimestamp(float(1610986013083)/1000.).strftime("%d %b %Y")
+    return date, cases, deaths, critical
 
 
 @client.event
@@ -56,8 +37,8 @@ async def on_message(message):
         return text.startswith('/krousmata') or text.startswith('/gkrouzmata') or client.user.mentioned_in(message)
 
     if should_send_data(message):
-        date, confirmed, deaths, intensive = get_data()
-        await message.channel.send(f'{date}: {confirmed} κρούσματα | {deaths} θάνατοι | {intensive} ΜΕΘ')
+        date, cases, deaths, critical = get_data()
+        await message.channel.send(f'{date}: {cases} κρούσματα | {deaths} θάνατοι | {critical} ΜΕΘ')
 
 
 if __name__ == "__main__":
